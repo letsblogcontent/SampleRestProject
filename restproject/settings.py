aceb3c04eaa4553922937f1174bc17b8cfc819cb
letsblogcontent <68807190+letsblogcontent@users.noreply.github.com>
2020-09-07 20:39:16 +0530

    Add files via upload

--- restproject/settings.py
+++ restproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -119,4 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
